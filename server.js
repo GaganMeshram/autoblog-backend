1b9cc65fccea18a0734c5c7efdb79bddcd7236a4
Gagan Meshram <gmeshram774@gmail.com>
2024-12-01 15:29:02 +0530

    added cors option

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const postsRoutes = require('./routes/posts');
 
+const corsOptions = {
+  origin: 'https://autoblog-frontend.vercel.app', // Replace with your actual frontend URL
+  methods: ['GET', 'POST'], // You can include more methods if needed
+};
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/posts', postsRoutes);
 
